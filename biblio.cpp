9f369f25ccdd374577d80fa1552046e6f8c5c235
TolottiMikael <45928162+TolottiMikael@users.noreply.github.com>
2018-12-21 12:26:53 -0200

    testes de compilação

---
+++ biblio.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print(){
+	std::cout<<"teste de inclusãod e bibliotecas"<< std::endl;
+}
---
+++ teste.cpp
@@ -0,0 +1,8 @@
+#include "biblio.cpp"
+
+int main(){
+
+   //Função guardada numa biblioteca estática
+   print();
+	system("pause");
+}
